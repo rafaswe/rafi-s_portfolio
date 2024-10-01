6388492df911cd84e97d58f7108ca7c0b997e4fd
Mahiya Rafa <mahiya35-2706@diu.edu.bd>
2024-10-01 15:08:10 +0600

    Added Blogs page

--- components/common/AnimateCard.tsx
+++ components/common/AnimateCard.tsx
@@ -4,12 +4,33 @@ import { ReactNode } from "react";
 const AnimateCard = ({
   variants,
   children,
+  className = "",
 }: {
-  variants: Variants;
+  variants?: Variants;
   children: ReactNode;
+  className?: string;
 }) => {
+  const linkVariants = {
+    initial: {
+      scale: 0.5,
+      opacity: 0,
+    },
+    animate: {
+      scale: 1,
+      opacity: 1,
+      transition: {
+        duration: 0.5,
+        ease: "easeOut",
+      },
+    },
+  };
   return (
-    <motion.div variants={variants} initial="initial" animate="animate">
+    <motion.div
+      variants={variants ?? linkVariants}
+      initial="initial"
+      animate="animate"
+      whileHover="hover"
+      className={className}>
       {children}
     </motion.div>
   );
--- components/showCaseSection/blogs/Blogs.container.tsx
+++ components/showCaseSection/blogs/Blogs.container.tsx
@@ -1,7 +1,38 @@
+import AnimateCard from "@/components/common/AnimateCard";
+import { Blogs } from "@/enum";
+import Image from "next/image";
+import Link from "next/link";
 import ComponentLayout from "../Component.layout";
 
 const BlogsContainer = () => {
-  return <ComponentLayout title="Blogs">BlogsContainer</ComponentLayout>;
+  return (
+    <ComponentLayout title="Blogs">
+      <div className="grid grid-cols-2 gap-6">
+        {Blogs?.map((blog, index) => (
+          <AnimateCard key={index} className="col-span-2 group lg:col-span-1">
+            <Link
+              href=""
+              className="bg-card  flex flex-col min-h-[385px]  lg:h-[500px] 2xl:h-[380px] rounded-lg shadow-lg">
+              <Image
+                src={blog?.imgUrl}
+                alt="blogImage"
+                width={1260}
+                height={760}
+                className="w-full h-52 object-cover rounded-lg"
+              />
+              <div className="p-3 flex flex-col gap-1">
+                <p className="text-xs opacity-60">Date: {blog.date}</p>
+                <p className="text-lg font-medium group-hover:text-active duration-300 transition-colors ease-in-out">
+                  {blog.title}
+                </p>
+                <p className="text-sm">{blog.shortDes}</p>
+              </div>
+            </Link>
+          </AnimateCard>
+        ))}
+      </div>
+    </ComponentLayout>
+  );
 };
 
 export default BlogsContainer;
